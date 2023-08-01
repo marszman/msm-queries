e8bdb222a0abbe81f2451a7acc82ad95f622ab62
marszman <136658905+marszman@users.noreply.github.com>
2023-08-01 03:09:58 +0000

    msm - directors complete

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,27 +1,25 @@
 class DirectorsController < ApplicationController
 
   def directors_page
-
-    
+    @director_data = Director.all
     render({ :template => "templates/director_table" })
   end
 
-  def youngest_directors_page
+  def director_selection
+    @director_data = Director.all
+    @movie_data = Movie.all
+    render({ :template => "templates/director_view" })
+  end
 
-    
+  def youngest_directors_page
+    @director_youngest_data = Director.all.order({ :dob => :desc })
+
     render({ :template => "templates/director_youngest_view" })
   end
 
   def eldest_directors_page
-
-    
+    @director_eldest_data = Director.all.order({ :dob => :asc })
     render({ :template => "templates/director_eldest_view" })
-  end
-
-  def director_selection
-
-    
-    render({ :template => "templates/director_view" })
   end
 
 end
